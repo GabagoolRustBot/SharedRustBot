d3333ec7e0e1246d7bbff75c67141bab49454ec7
RyanD333 <104323615+RyanD333@users.noreply.github.com>
2022-06-12 22:10:31 -0400

    Update rustBot.js

--- rustBot.js
+++ rustBot.js
@@ -17,7 +17,8 @@
 
 console.log("Starting");
 const RustPlus = require('@liamcottle/rustplus.js');
-var rustplus = new RustPlus('208.52.152.118', '28145', '76561198124218884', '-443236188');
+var rustplus = new RustPlus('208.52.152.118', '28145', '76561198050799967', '-841804922'); // Ryan
+//var rustplus = new RustPlus('208.52.152.118', '28145', '76561198124218884', '-443236188'); // Twix
 const samSwitches = ['26618389', '11276478', '26675114', '9229886', '26034625']
 const turretSwitches = ['9331004','9332360', '9321241', '9334053', '9325142', 
                         '20002729',

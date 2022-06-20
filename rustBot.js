604088091787577d007460f9b2b3d456532a63b7
RyanD333 <104323615+RyanD333@users.noreply.github.com>
2022-06-20 16:46:09 -0400

    JSON Server Configs, Command Key, and Refactor
    -Added real time server info, user info, and ID storage via JSON files
    
    -Made generic chat command key vs using hardcoded "!" for server compatibility
    
    -Refactored command section into generic helper function for simplicity

--- rustBot.js
+++ rustBot.js
@@ -1,136 +1,131 @@
-//Topia Small = 0.0.0.0:28076 = hostname
 //Scan for messages command: npx @liamcottle/rustplus.js fcm-listen
 
-//MAIN SWITCHES
-//BEDROOMS starting with Ryan/Twix and going clockwise
-//r/t: 9331004 -> (sam, no turret) -> 9332360 -> 9321241 -> 9334053 -> 9325142
-//BEDROOM FLOOR OUTER RING
-//r/t: (Lights) 10723613 -> (Heaters) 76576392 -> (Turrets) 20002729
+const process = require('process');
+const RustPlus = require("@liamcottle/rustplus.js");
+const userData = require("./user.json")
 
-//GATE SWITCHES
-//Keos L: Turret 11311303 R: Sam 26618389, Nothing 26618579
-//1 L: Turret 17411035 R: Turret 12496557, Middle: Heaters and lights for gate 1 ***
-//2 L: Turret 11274989 R: SAM 11276478, Nothing 26646402
-//3 L: Turret 17404490 R: Nothing 20239610
-//4 L: Turret 11328218 R: SAM 26675114, Nothing 26675021
-//5 L: Turret 13006497 R: Nothing 11545782
+//String for name of JSON file being read (as opposed to hardcoding), can be changed via arguments later
+var pathString = "testData.json"//Using hardcoding for testing
+//var pathString = process.argv[2]//First arg in command line call = path to json, or just filename if in root
+var jData = require("./" + pathString)
 
+//Outer scope declaration of ID arrays
+var turretSwitches
+var samSwitches
+var lightSwitches
+var heatSwitches
+var StorageMonitors
+
+//Command key for servers where ! is being used, define in each server JSON and assign here isntead
+var commandKey = jData.serverCommandKey
+
+//Fucntion for pulling info from JSON, ran before any switch array calls for most recent data
+function readData(){
+  //Delete old data from cache
+  delete require.cache[require.resolve("./" + pathString)]
+  //Load new data
+  jData = require("./" + pathString)
+  //Assign new data
+  turretSwitches = jData.turretSwitches
+  samSwitches = jData.samSwitches
+  lightSwitches = jData.lightSwitches
+  heatSwitches = jData.heatSwitches
+  StorageMonitors = jData.StorageMonitors
+}
+
+//Helper function for switch calls, takes switch type as string, and on/off as 1/0
+function genericSwitch(switchType, switchPosition){
+  var targetArr = []
+  switch(switchType){
+    case "Turrets":
+      targetArr = turretSwitches
+      break
+    case "SAMs":
+      targetArr = samSwitches
+      break
+    case "Lights":
+      targetArr = lightSwitches
+      break
+    case "Heaters":
+      targetArr = heatSwitches
+      break
+    //TODO error catch maybe
+  }
+  switch(switchPosition){
+    case 0:
+      for(var i = 0; i < targetArr.length; i++){
+        rustplus.turnSmartSwitchOff(targetArr[i], (message) => {
+          //TODO add error catch from message
+          return true;
+        });
+      }
+      rustplus.sendTeamMessage("BOT: " + switchType + " off");
+      break
+    case 1: 
+      for(var i = 0; i < targetArr.length; i++){
+        rustplus.turnSmartSwitchOn(targetArr[i], (message) => {
+          //TODO add error catch from message
+          return true;
+        });
+      }
+      rustplus.sendTeamMessage("BOT: " + switchType + " on");
+      break      
+  }
+}
+
+//BOT START----------------------------------
+
+//JSON server data used instead of hardcoding 
+var rustplus = new RustPlus(jData.serverIP, jData.serverPort, userData.userID, userData.userToken);
+
+//Connect to rust server
 console.log("Starting");
-const RustPlus = require('@liamcottle/rustplus.js');
-//var rustplus = new RustPlus('208.52.152.118', '28145', '76561198050799967', '-841804922'); // Ryan
-var rustplus = new RustPlus('208.52.152.118', '28145', '76561198124218884', '-443236188'); // Twix
-
-//Entity IDs for interactable in-game objects. 
-const samSwitches = ['26618389', '11276478', '26675114', '9229886', '26034625']
-const turretSwitches = ['9331004','9332360', '9321241', '9334053', '9325142', 
-                        '20002729',
-                        '11311303', '17411035', '12496557', '11274989', '17404490', '11328218', '13006497']
-const lightSwitches = ['10723613']
-const heatSwitches = ['76576392']
-const StorageMonitors = ['82964708', '76132494', '82972481', '82974226', '82975650', '82978546', '82980151']
-
-// connect to rust server
 rustplus.connect();
 
 // wait until connected before sending commands
-rustplus.on('connected', () => {
+rustplus.on("connected", () => {
   console.log("Connected");
   rustplus.sendTeamMessage("BOT: Bot connected");  
   // ready to send requests
+  readData()
 });
 
 //Bot Commands
-rustplus.on('message', (message) => {
-  //Unused 
-  var hitCheck = false
-  var stateCounter = 0
+rustplus.on("message", (message) => {
+  //Refresh data, "brute force" placement here, refreshing after every message, 
+  //can be placed inside command calls to lighten loads if need be
+  readData()
   //Message event JSON from broadcast, as string
   var str = JSON.stringify(message);
-  //SAM------------------------------------
-  if(str.includes('!samon')){
-    //hitCheck = true
-    for(var i = 0; i < samSwitches.length; i++){
-      rustplus.turnSmartSwitchOn(samSwitches[i], (message) => {
-        //console.log("turnSmartSwitchOn response message: " + JSON.stringify(message));
-        return true;
-      });
-    }
-    rustplus.sendTeamMessage("BOT: SAM switches on");
+  //SAM
+  if(str.includes(commandKey + "samon")){
+    genericSwitch("SAMs", 1)
   }
-  else if(str.includes('!samoff')){
-    //hitCheck = true
-    for(var i = 0; i < samSwitches.length; i++){
-      rustplus.turnSmartSwitchOff(samSwitches[i], (message) => {
-        //console.log("turnSmartSwitchOn response message: " + JSON.stringify(message));
-        return true;
-      });
-    }
-    rustplus.sendTeamMessage("BOT: SAM switches off");
+  else if(str.includes(commandKey + "samoff")){
+    genericSwitch("SAMs", 0)
   }
-  //TURRETS----------------------------------
-  else if(str.includes('!tton')){
-    //hitCheck = true
-    for(var i = 0; i < turretSwitches.length; i++){
-      rustplus.turnSmartSwitchOn(turretSwitches[i], (message) => {
-        //console.log("turnSmartSwitchOn response message: " + JSON.stringify(message));
-        return true;
-      });
-    }
-    rustplus.sendTeamMessage("BOT: Turret switches on");
+  //TURRETS
+  else if(str.includes(commandKey + "tton")){
+    genericSwitch("Turrets", 1)
   }
-  else if(str.includes('!ttoff')){
-    //hitCheck = true
-    for(var i = 0; i < turretSwitches.length; i++){
-      rustplus.turnSmartSwitchOff(turretSwitches[i], (message) => {
-        //console.log("turnSmartSwitchOn response message: " + JSON.stringify(message));
-        return true;
-      });
-    }
-    rustplus.sendTeamMessage("BOT: Turret switches off");
+  else if(str.includes(commandKey + "ttoff")){
+    genericSwitch("Turrets", 0)
   }
-  //LIGHTS----------------------------------
-  else if(str.includes('!lightson')){
-    //hitCheck = true
-    for(var i = 0; i < lightSwitches.length; i++){
-      rustplus.turnSmartSwitchOn(lightSwitches[i], (message) => {
-        //console.log("turnSmartSwitchOn response message: " + JSON.stringify(message));
-        return true;
-      });
-    }
-    rustplus.sendTeamMessage("BOT: Light switches on");
+  //LIGHTS
+  else if(str.includes(commandKey + "lightson")){
+    genericSwitch("Lights", 1)
   }
-  else if(str.includes('!lightsoff')){
-    //hitCheck = true
-    for(var i = 0; i < lightSwitches.length; i++){
-      rustplus.turnSmartSwitchOff(lightSwitches[i], (message) => {
-        //console.log("turnSmartSwitchOn response message: " + JSON.stringify(message));
-        return true;
-      });
-    }
-    rustplus.sendTeamMessage("BOT: Light switches off");
+  else if(str.includes(commandKey + "lightsoff")){
+    genericSwitch("Lights", 0)
   }
-  //HEATERS--------------------------------------
-  else if(str.includes('!heaterson')){
-    //hitCheck = true
-    for(var i = 0; i < heatSwitches.length; i++){
-      rustplus.turnSmartSwitchOn(heatSwitches[i], (message) => {
-        //console.log("turnSmartSwitchOn response message: " + JSON.stringify(message));
-        return true;
-      });
-    }
-    rustplus.sendTeamMessage("BOT: Heater switches on");
+  //HEATERS
+  else if(str.includes(commandKey + "heaterson")){
+    genericSwitch("Heaters", 1)
   }
-  else if(str.includes('!heatersoff')){
-    //hitCheck = true
-    for(var i = 0; i < heatSwitches.length; i++){
-      rustplus.turnSmartSwitchOff(heatSwitches[i], (message) => {
-        //console.log("turnSmartSwitchOn response message: " + JSON.stringify(message));
-        return true;
-      });
-    }
-    rustplus.sendTeamMessage("BOT: Heater switches off");
+  else if(str.includes(commandKey + "heatersoff")){
+    genericSwitch("Heaters", 0)
   }
-  else if(str.includes('!upkeep')){
+  else if(str.includes(commandKey + "upkeep")){
     for(let i = 0; i < StorageMonitors.length; ++i){
       rustplus.getEntityInfo(StorageMonitors[i], (mess) =>{
         var st = JSON.stringify(mess);
@@ -153,7 +148,7 @@ rustplus.on('message', (message) => {
         let hours = Math.floor((decayTime - days * day) / hour);
         let minutes = Math.floor((decayTime - days * day - hours * hour) / minute);
         let seconds = decayTime - days * day - hours * hour - minutes * minute;
-        //I want to put everything above this into a helper function but I'm not sure how to do that in NodeJS yet. 
+        //I want to put everything above this into a helper function but I"m not sure how to do that in NodeJS yet. 
         if(i == 0){
           rustplus.sendTeamMessage("MAIN TC:    " + days + "D " + hours + "H " + minutes + "M " + seconds + "S");
         } else{
@@ -165,62 +160,13 @@ rustplus.on('message', (message) => {
     }
     
   }
-  else if(str.includes('!rockets')){
+  else if(str.includes(commandKey + "rockets")){
     rustplus.sendTeamMessage("BOT: The rockets exist");
   }
-  else if(str.includes('!bot')){
+  else if(str.includes(commandKey + "bot")){
     rustplus.sendTeamMessage("BOT: Bot is connected");
   }
-  else if(str.includes('!help')){
+  else if(str.includes(commandKey + "help")){
     rustplus.sendTeamMessage("BOT: Commands: TODO");
   }
-  //DISABLED: If a command was given, confirm switch states and send message with results
-  /*if(hitCheck){
-    for(var i = 0; i < samSwitches.length; i++){
-      rustplus.getEntityInfo(samSwitches[i], (message) => {
-        //TODO fix this parse with path JSON.response.entityinfo.payload.value (maybe)
-        if(JSON.parse(message).response.entityinfo.payload.value){
-          stateCounter += 1
-        }
-        return true;
-      });
-    }
-    rustplus.sendTeamMessage("BOT: " + stateCounter + " SAMs are turned on");
-  }*/
-  
 });
-//Storage Monitor changes
-// rustplus.on('message', (message) => {
-//   if(message.broadcast && message.broadcast.entityChanged){
-//       var entityChanged = message.broadcast.entityChanged;
-  
-//       var entityId = entityChanged.entityId;
-//       var value = entityChanged.payload.value;
-//       var capacity = entityChanged.payload.capacity;
-//       var items = entityChanged.payload.items;
-      
-//       // only print info when second broadcast is received
-//       if(!value){
-
-//           console.log(`entity ${entityId} has a capacity of ${capacity}`);
-//           console.log(`entity ${entityId} contains ${items.length} item(s)`);
-          
-//           // print out the items in this storage entity
-//           items.forEach((item) => {
-//               console.log(item);
-//           });
-//       }
-//   }
-// });
-//Log all messages
-//rustplus.on('message', (message) => {
-  //console.log("message received: " + JSON.stringify(message));
-//});
-
-//rustplus.getEntityInfo('11274989', (message) => {
-  //console.log("Switch")
-  //console.log("getEntityInfo response message: " + JSON.stringify(message));
-  //return true;
-//});
-
-

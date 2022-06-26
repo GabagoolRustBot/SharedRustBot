eaffd9679172d9d33e322a28ed6a24102cc46169
RyanD333 <104323615+RyanD333@users.noreply.github.com>
2022-06-26 19:08:53 -0400

    Update rustBot.js
    Added boton/botoff and raid commands
    
    Can now use checkSender function for owner only commands

--- rustBot.js
+++ rustBot.js
@@ -14,6 +14,9 @@ var jData = require("./" + pathString)
 //Config JSON if needed later
 var configJSON = require("./rustplus.config.json")
 
+//Global boolean for tracking if bot is paused, starts unpaused
+var paused = false
+
 //Outer scope declaration of ID arrays
 var turretSwitches
 var samSwitches
@@ -25,7 +28,7 @@ var StorageMonitors
 var commandKey = jData.serverCommandKey
 
 //Fucntion for pulling info from JSON, ran before any switch array calls for most recent data
-function readData(){
+function readData() {
   //Delete old data from cache
   delete require.cache[require.resolve("./" + pathString)]
   //Load new data
@@ -39,9 +42,9 @@ function readData(){
 }
 
 //Helper function for switch calls, takes switch type as string, and on/off as 1/0
-function genericSwitch(switchType, switchPosition){
+function genericSwitch(switchType, switchPosition) {
   var targetArr = []
-  switch(switchType){
+  switch (switchType) {
     case "Turrets":
       targetArr = turretSwitches
       break
@@ -56,9 +59,9 @@ function genericSwitch(switchType, switchPosition){
       break
     //TODO error catch maybe
   }
-  switch(switchPosition){
+  switch (switchPosition) {
     case 0:
-      for(var i = 0; i < targetArr.length; i++){
+      for (var i = 0; i < targetArr.length; i++) {
         rustplus.turnSmartSwitchOff(targetArr[i], (message) => {
           //TODO add error catch from message
           return true;
@@ -66,53 +69,64 @@ function genericSwitch(switchType, switchPosition){
       }
       rustplus.sendTeamMessage("BOT: " + switchType + " off");
       break
-    case 1: 
-      for(var i = 0; i < targetArr.length; i++){
+    case 1:
+      for (var i = 0; i < targetArr.length; i++) {
         rustplus.turnSmartSwitchOn(targetArr[i], (message) => {
           //TODO add error catch from message
           return true;
         });
       }
       rustplus.sendTeamMessage("BOT: " + switchType + " on");
-      break      
+      break
   }
 }
 
 //Append data to JSON. Takes data to append and the key to append to as string (TurretSwitches etc). 
-function appendJSON (newData, key){
-  fs.readFile("./" + pathString, 'utf8', function readFileCallback(err, data){
-    if (err){
+function appendJSON(newData, key) {
+  fs.readFile("./" + pathString, 'utf8', function readFileCallback(err, data) {
+    if (err) {
       console.log(err);
     }
-    else{
-    obj = JSON.parse(data); 
-    //Switch keys in JSON for match
-    switch(key){
-      case "Turrets":
-        obj.turretSwitches.push(newData);
-        break
-      case "SAMs":
-        obj.samSwitches.push(newData);
-        break
-      case "Lights":
-        obj.lightSwitches.push(newData);
-        break
-      case "Heaters":
-        obj.heaterSwitches.push(newData);
-        break
-      case "Storage":
-        obj.storageMonitors.push(newData);
-        break
+    else {
+      obj = JSON.parse(data);
+      //Switch keys in JSON for match
+      switch (key) {
+        case "Turrets":
+          obj.turretSwitches.push(newData);
+          break
+        case "SAMs":
+          obj.samSwitches.push(newData);
+          break
+        case "Lights":
+          obj.lightSwitches.push(newData);
+          break
+        case "Heaters":
+          obj.heaterSwitches.push(newData);
+          break
+        case "Storage":
+          obj.storageMonitors.push(newData);
+          break
+      }
+      json = JSON.stringify(obj); //convert it back to json
+      fs.writeFile("./" + pathString, json, 'utf8', function () { x => True }); // write it back 
     }
-    json = JSON.stringify(obj); //convert it back to json
-    fs.writeFile("./" + pathString, json, 'utf8', function(){x => True}); // write it back 
-  }});
-  console.log("Wrote "+newData+" to "+key)
+  });
+  console.log("Wrote " + newData + " to " + key)
+}
+
+//Check to see if the message was sent by the bot owner
+function checkSender(message){
+  if(message.includes(userData.userID)){
+    return true
+  }
+  else{
+    return false
+  }
 }
 
 //BOT START----------------------------------
 //Banner
-assets.banner.forEach(x => {console.log(x)})
+assets.banner.forEach(x => { console.log(x) })
 
 //JSON server data used instead of hardcoding 
 var rustplus = new RustPlus(jData.serverIP, jData.serverPort, userData.userID, userData.userToken);
@@ -124,7 +138,7 @@ rustplus.connect();
 // wait until connected before sending commands
 rustplus.on("connected", () => {
   console.log("Connected");
-  rustplus.sendTeamMessage("BOT: Bot connected");  
+  rustplus.sendTeamMessage("BOT: Bot connected");
   // ready to send requests
   readData()
 });
@@ -136,76 +150,112 @@ rustplus.on("message", (message) => {
   readData()
   //Message event JSON from broadcast, as string
   var str = JSON.stringify(message);
-  //SAM
-  if(str.includes(commandKey + "samon")){
-    genericSwitch("SAMs", 1)
-  }
-  else if(str.includes(commandKey + "samoff")){
-    genericSwitch("SAMs", 0)
-  }
-  //TURRETS
-  else if(str.includes(commandKey + "tton")){
-    genericSwitch("Turrets", 1)
-  }
-  else if(str.includes(commandKey + "ttoff")){
-    genericSwitch("Turrets", 0)
-  }
-  //LIGHTS
-  else if(str.includes(commandKey + "lightson")){
-    genericSwitch("Lights", 1)
-  }
-  else if(str.includes(commandKey + "lightsoff")){
-    genericSwitch("Lights", 0)
-  }
-  //HEATERS
-  else if(str.includes(commandKey + "heaterson")){
-    genericSwitch("Heaters", 1)
-  }
-  else if(str.includes(commandKey + "heatersoff")){
-    genericSwitch("Heaters", 0)
-  }
-  else if(str.includes(commandKey + "upkeep")){
-    for(let i = 0; i < StorageMonitors.length; ++i){
-      rustplus.getEntityInfo(StorageMonitors[i], (mess) =>{
-        var st = JSON.stringify(mess);
-        //console.log(st);
-
-        var start = st.search("protectionExpiry");
-        st = st.slice(start);
-        var end = st.search("}");
-        st = st.slice(18, end); //Hard Coded, yes I know its bad. 
-
-        var rawTime = parseInt(st);
-        //console.log(st);
-
-        let day = 86400;
-        let hour = 3600;
-        let minute = 60;
-
-        let decayTime = Math.floor((rawTime*1000 - Date.now()) / 1000);
-        let days = Math.floor(decayTime / day);
-        let hours = Math.floor((decayTime - days * day) / hour);
-        let minutes = Math.floor((decayTime - days * day - hours * hour) / minute);
-        let seconds = decayTime - days * day - hours * hour - minutes * minute;
-        //I want to put everything above this into a helper function but I"m not sure how to do that in NodeJS yet. 
-        if(i == 0){
-          rustplus.sendTeamMessage("MAIN TC:    " + days + "D " + hours + "H " + minutes + "M " + seconds + "S");
-        } else{
-          rustplus.sendTeamMessage("GATE " + (i) + ":      " + days + "D " + hours + "H " + minutes + "M " + seconds + "S");
-        }
-        
-      });
-          
+  //console.log(str)
+  //Block if paused
+  if (!paused) {
+    //SAM
+    if (str.includes(commandKey + "samon")) {
+      genericSwitch("SAMs", 1)
     }
-    
+    else if (str.includes(commandKey + "samoff")) {
+      genericSwitch("SAMs", 0)
+    }
+    //TURRETS
+    else if (str.includes(commandKey + "tton")) {
+      genericSwitch("Turrets", 1)
+    }
+    else if (str.includes(commandKey + "ttoff")) {
+      genericSwitch("Turrets", 0)
+    }
+    //LIGHTS
+    else if (str.includes(commandKey + "lightson")) {
+      genericSwitch("Lights", 1)
+    }
+    else if (str.includes(commandKey + "lightsoff")) {
+      genericSwitch("Lights", 0)
+    }
+    //HEATERS
+    else if (str.includes(commandKey + "heaterson")) {
+      genericSwitch("Heaters", 1)
+    }
+    else if (str.includes(commandKey + "heatersoff")) {
+      genericSwitch("Heaters", 0)
+    }
+    else if (str.includes(commandKey + "upkeep")) {
+      for (let i = 0; i < StorageMonitors.length; ++i) {
+        rustplus.getEntityInfo(StorageMonitors[i], (mess) => {
+          var st = JSON.stringify(mess);
+          //console.log(st);
+
+          var start = st.search("protectionExpiry");
+          st = st.slice(start);
+          var end = st.search("}");
+          st = st.slice(18, end); //Hard Coded, yes I know its bad. 
+
+          var rawTime = parseInt(st);
+          //console.log(st);
+
+          let day = 86400;
+          let hour = 3600;
+          let minute = 60;
+
+          let decayTime = Math.floor((rawTime * 1000 - Date.now()) / 1000);
+          let days = Math.floor(decayTime / day);
+          let hours = Math.floor((decayTime - days * day) / hour);
+          let minutes = Math.floor((decayTime - days * day - hours * hour) / minute);
+          let seconds = decayTime - days * day - hours * hour - minutes * minute;
+          //I want to put everything above this into a helper function but I"m not sure how to do that in NodeJS yet. 
+          if (i == 0) {
+            rustplus.sendTeamMessage("MAIN TC:    " + days + "D " + hours + "H " + minutes + "M " + seconds + "S");
+          } else {
+            rustplus.sendTeamMessage("GATE " + (i) + ":      " + days + "D " + hours + "H " + minutes + "M " + seconds + "S");
+          }
+
+        });
+
+      }
+
+    }
+    else if (str.includes(commandKey + "rockets")) {
+      rustplus.sendTeamMessage("BOT: The rockets exist");
+    }
+    else if (str.includes(commandKey + "status")) {
+      rustplus.sendTeamMessage("BOT: Bot is connected and on");
+    }
+    else if (str.includes(commandKey + "raid")) {
+      genericSwitch("SAMs", 1)
+      genericSwitch("Turrets", 1)
+      genericSwitch("Heaters", 1)
+      genericSwitch("Lightss", 1)
+      rustplus.sendTeamMessage("BOT: Everything is on. Godspeed.");
+    }
+    else if (str.includes(commandKey + "help")) {
+      rustplus.sendTeamMessage("BOT: Commands: TODO");
+    }
+    else if(str.includes(commandKey + "botoff")){
+      if(checkSender(str)){
+        paused = true
+        rustplus.sendTeamMessage("BOT: Bot is off");
+      }
+    }
   }
-  else if(str.includes(commandKey + "rockets")){
-    rustplus.sendTeamMessage("BOT: The rockets exist");
-  }
-  else if(str.includes(commandKey + "bot")){
-    rustplus.sendTeamMessage("BOT: Bot is connected");
-  }
-  else if(str.includes(commandKey + "help")){
-    rustplus.sendTeamMessage("BOT: Commands: TODO");
+  //If paused, still process these commands
+  else {
+    if(str.includes(commandKey + "status")) {
+      rustplus.sendTeamMessage("BOT: Bot is connected but off. To turn on use 'boton'")
+    }
+    else if(str.includes(commandKey + "boton")){
+      if(checkSender(str)){
+        paused = false
+        rustplus.sendTeamMessage("BOT: Bot is on");
+      }
+    }
+    else if (str.includes(commandKey + "raid")) {
+      genericSwitch("SAMs", 1)
+      genericSwitch("Turrets", 1)
+      genericSwitch("Heaters", 1)
+      genericSwitch("Lightss", 1)
+      rustplus.sendTeamMessage("BOT: Everything is on. Godspeed.");
+    }
   }
 });
